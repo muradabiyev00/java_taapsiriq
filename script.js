20634a9834a7a7d1654c0b9e95d73422ea8e6e8c
Murad Abiyev <muradabiyev20@mail.ru>
2024-04-24 09:14:01 +0400

    tapsiriq

---
+++ script.js
@@ -0,0 +1,31 @@
+// // birinci
+// let string1 = 'utu';
+// let string2 = string1.split('').reverse().join('');
+// console.log(string2);
+// if(string1===string2){
+//     console.log("Polindiromdur");
+// }else{
+//     console.log("polindrom deyil");
+// }
+// ikinci
+// let string= 'Menim adim Muraddir';
+// let stringarr=string.split('');
+// console.log(stringarr);
+
+// if(string.length >10 && string.length<20){
+//     console.log("true")
+// }else{
+//     console.log("false")
+// }
+// // ucuncu
+// let arr=[5,10,15,20]
+// console.log(arr);
+// let say = arr.length;
+// console.log(say);
+
+// let cem = 0;
+// for(let i=0 ;i<say; i++){
+//     cem += arr[1];
+// }
+// let edediorta =cem/say;
+// console.log(edediorta);
